bf0c65d022ed4e3dbb0042a40d83e917e81fb3fe
Penn Oser <pennoser@gmail.com>
2018-03-08 10:40:47 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,19 +7,34 @@ cp Ubuntu_Setup/.vimrc ~/
 cp Ubuntu_Setup/.zshrc ~/
 rm -rf Ubuntu_Setup
 cd ~
-pip3 install powerline-status
-sudo pip3 install powerline-status
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-mkdir ~/.tmux
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle
-git clone git://github.com/altercation/vim-colors-solarized.git
-git clone git://github.com/airblade/vim-gitgutter.git
-git clone git://github.com/ervandew/supertab
-git clone git://github.com/SirVer/ultisnips
-git clone https://github.com/honza/vim-snippets.git
-git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-git config --global core.pager cat
-cd ~
-
+if [[ -d ~/.vim/bundle ]]
+	cd ~/.oh-my-zsh/custom/themes/powerlevel9k
+	git pull
+	cd ~/.vim/bundle/supertab
+	git pull
+	cd ~/.vim/bundle/syntastic
+	git pull
+	cd ~/.vim/bundle/ultisnips
+	git pull
+        cd ~/.vim/bundle/vim-colors-solarized
+	git pull
+	cd ~/.vim/bundle/vim-gitgutter
+	git pull
+	cd ~/.vim/bundle/vim-snippets
+	git pull
+else
+	pip3 install powerline-status
+	sudo pip3 install powerline-status
+	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	cd ~/.vim/bundle
+	git clone git://github.com/altercation/vim-colors-solarized.git
+	git clone git://github.com/airblade/vim-gitgutter.git
+	git clone git://github.com/ervandew/supertab
+	git clone git://github.com/SirVer/ultisnips
+	git clone https://github.com/honza/vim-snippets.git
+	git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+	git config --global core.pager cat
+	cd ~
+fi
