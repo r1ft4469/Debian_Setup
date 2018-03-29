576dea44295454d32324c0590e8280f822490dc2
Penn Oser <r1ft4469@users.noreply.github.com>
2018-03-28 21:33:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cd /tmp
+cd ~/
 git clone https://github.com/r1ft4469/Debian_Setup
-cp /tmp/Debain_Setup/.tmux.conf ~/
-cp /tmp/Debain_Setup/.vimrc ~/
-cp /tmp/Debain_Setup/.zshrc ~/
-rm -rf /tmp/Debain_Setup
+mkdir ~/.tmux
+cp Debian_Setup/.tmux.conf ~/
+cp Debian_Setup/.vimrc ~/
+cp Debian_Setup/.zshrc ~/
+cp Debian_Setup/tmux_split_pane.sh ~/.tmux/
+rm -rf Debain_Setup
 cd ~
 if [ -d ~/.vim/bundle ]
 then
@@ -25,12 +27,13 @@ then
 	git pull
 	cd ~/.vim/bundle/vim-snippets
 	git pull
+	cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git pull
+	source .zshrc
 else
 	pip3 install powerline-status
 	sudo pip3 install powerline-status
 	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-	mkdir -p ~/.tmux
-	git clone https://github.com/nhdaly/tmux-better-mouse-mode ~/.tmux
 	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 	cd ~/.vim/bundle
