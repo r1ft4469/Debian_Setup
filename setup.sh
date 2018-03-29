4b8b0bd7abd613d81f3d82b5c2e87c05b7c5d268
Penn Oser <r1ft4469@users.noreply.github.com>
2018-03-29 12:42:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,5 +46,7 @@ else
 	git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git config --global core.pager cat
+	if "test ! -d ~/.tmux/plugins/tpm" \
+	   "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins'"
 	cd ~
 fi
