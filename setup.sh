e23571e4a0dfc5b7da92cae46ca39b0240a51e1d
Penn Oser <r1ft4469@users.noreply.github.com>
2018-03-28 20:44:25 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,6 +40,7 @@ else
 	git clone git://github.com/SirVer/ultisnips
 	git clone https://github.com/honza/vim-snippets.git
 	git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git config --global core.pager cat
 	cd ~
 fi
